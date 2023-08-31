afbd05bbb290ca989f0841a3f9b2f1eb50f4e84d
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2023-08-31 23:36:05 +0700

    fixed

---
+++ PracticeMidTerm/src/test/java/com/example/practicemidterm/PracticeMidTermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicemidterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMidTermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
